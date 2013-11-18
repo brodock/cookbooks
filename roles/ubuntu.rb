01139bbe5b2f76876f970549f0dfdc718df60a42
Gabriel Mazetto <brodock@gmail.com>
2013-11-18 03:17:40 -0200

    Added htop to ubuntu role and omnibus updater
    This will make sure chef-client is always up to date.

--- roles/ubuntu.rb
+++ roles/ubuntu.rb
@@ -1,6 +1,7 @@
 name "ubuntu"
 description "Ubuntu Server essential applications and configurations"
-run_list 'recipe[apt]', 'recipe[build-essential]',  'recipe[dpkg_packages]', 'recipe[git]', 'recipe[locale]', 'recipe[vim]'
+run_list 'recipe[apt]', 'recipe[build-essential]',  'recipe[dpkg_packages]',
+         'recipe[git]', 'recipe[htop]', 'recipe[locale]', 'recipe[omnibus_updater]', 'recipe[vim]'
 default_attributes(
     "locale" => {
         :lang => 'pt_BR.utf8',
